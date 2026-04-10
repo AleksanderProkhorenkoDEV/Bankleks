478e49944845e9f60204285eac908ce28b108b02
aleksander <aprokhorenkodev@gmail.com>
2026-04-10 14:02:50 +0200

    feat: add register endpoint

--- back/src/main/java/com/example/back/controllers/AuthController.java
+++ back/src/main/java/com/example/back/controllers/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.back.dto.auth.LoginRequestDTO;
 import com.example.back.dto.auth.LoginResponseDTO;
+import com.example.back.dto.auth.RegisterRequestDTO;
+import com.example.back.entities.user.User;
+import com.example.back.repositories.UserRepository;
 import com.example.back.services.JwtService;
 
 @RestController
@@ -20,10 +24,14 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtService jwtService) {
+    public AuthController(AuthenticationManager authenticationManager, JwtService jwtService, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/login")
@@ -34,8 +42,29 @@ public class AuthController {
     }
 
     private UserDetails authenticate(LoginRequestDTO request) {
-        Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        if(!(auth.getPrincipal() instanceof UserDetails user)) throw new IllegalStateException("Unexpected type");  
+        Authentication auth = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        if (!(auth.getPrincipal() instanceof UserDetails user))
+            throw new IllegalStateException("Unexpected type");
         return user;
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequestDTO request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("El email ya está registrado");
+        }      
+
+        userRepository.save(createUser(request));
+
+        return ResponseEntity.ok("Usuario registrado correctamente");
+    }
+
+    private User createUser(RegisterRequestDTO request){
+        return new User(request.getEmail(), request.getName(), passwordEncoder.encode(request.getPassword()));
+    }
+
 }
