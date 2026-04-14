30b7dcc7cdb5d071ef46d890f44e904c333ceb6f
aleksander <aprokhorenkodev@gmail.com>
2026-04-14 15:41:51 +0200

    feat: add validation and constructor for registerDTO

--- back/src/main/java/com/example/back/dto/auth/RegisterRequestDTO.java
+++ back/src/main/java/com/example/back/dto/auth/RegisterRequestDTO.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class RegisterRequestDTO {
 
     @Email(message = "{validation.email}")
@@ -17,27 +19,13 @@ public class RegisterRequestDTO {
     @Size(min = 8, message = "{validation.password.size}")
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
+    public RegisterRequestDTO(
+            @Email(message = "{validation.email}") @NotEmpty(message = "{validation.notEmpty}") String email,
+            @NotEmpty(message = "{validation.notEmpty}") String name,
+            @NotBlank(message = "{validation.notBlank}") @Size(min = 8, message = "{validation.password.size}") String password) {
         this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
+
 }
