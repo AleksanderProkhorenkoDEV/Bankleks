442406e3c9da1023b3e26487641ab7b3ce603b39
aleksander <aprokhorenkodev@gmail.com>
2026-04-15 11:52:31 +0200

    fix: error forgiven autoincrmental value for ID

---
+++ back/src/main/java/com/example/back/entities/auth/Role.java
@@ -0,0 +1,22 @@
+package com.example.back.entities.auth;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
--- back/src/main/java/com/example/back/entities/user/User.java
+++ back/src/main/java/com/example/back/entities/user/User.java
@@ -1,8 +1,15 @@
 package com.example.back.entities.user;
 
+import com.example.back.entities.auth.Role;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
@@ -10,18 +17,29 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
 
     public User() {
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, Role rol) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.role = rol;
     }
 }
