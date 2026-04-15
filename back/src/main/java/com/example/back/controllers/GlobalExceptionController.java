a6a3fc6125883f473a7209d2784688193f507995
aleksander <aprokhorenkodev@gmail.com>
2026-04-15 16:27:09 +0200

    feat: capture new exception

--- back/src/main/java/com/example/back/controllers/GlobalExceptionController.java
+++ back/src/main/java/com/example/back/controllers/GlobalExceptionController.java
@@ -12,28 +12,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.back.dto.ErrorResponseDTO;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 public class GlobalExceptionController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleValidationErrors(MethodArgumentNotValidException ex) {
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ErrorResponseDTO> handleValidationErrors(MethodArgumentNotValidException ex) {
 
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors()
-                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+                Map<String, String> errors = new HashMap<>();
+                ex.getBindingResult().getFieldErrors()
+                                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
-        return ResponseEntity.badRequest().body(
-                new ErrorResponseDTO(
-                        "Error de validación",
-                        HttpStatus.BAD_REQUEST.value(),
-                        errors));
+                return ResponseEntity.badRequest().body(
+                                new ErrorResponseDTO(
+                                                "Error de validación",
+                                                HttpStatus.BAD_REQUEST.value(),
+                                                errors));
 
-    }
+        }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponseDTO> handleBadCredentialsError() {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseDTO("Credenciales no validas", HttpStatus.UNAUTHORIZED.value()));
-    }
+        @ExceptionHandler(BadCredentialsException.class)
+        public ResponseEntity<ErrorResponseDTO> handleBadCredentialsError() {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                                .body(new ErrorResponseDTO("Credenciales no validas", HttpStatus.UNAUTHORIZED.value()));
+        }
 
+        @ExceptionHandler(EntityNotFoundException.class)
+        public ResponseEntity<ErrorResponseDTO> handleEntityNotFoundError() {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                                .body(new ErrorResponseDTO("Entidad no encontrada", HttpStatus.NOT_FOUND.value()));
+        }
 }
