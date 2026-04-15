9f4901e91a7f268b939d3f9d39d22e77e6fa054d
aleksander <aprokhorenkodev@gmail.com>
2026-04-15 16:44:52 +0200

    refactor: restore the overrideing methds.

--- back/src/main/java/com/example/back/repositories/RefreshTokenRepository.java
+++ back/src/main/java/com/example/back/repositories/RefreshTokenRepository.java
@@ -3,5 +3,9 @@ package com.example.back.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.back.entities.auth.RefreshToken;
+import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {}
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+}
