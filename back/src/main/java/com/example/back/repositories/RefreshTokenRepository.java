f8a9f75dff94873d8f61ce30b3586e7a867251b4
aleksander <aprokhorenkodev@gmail.com>
2026-04-15 16:48:46 +0200

    fix: error when i delete the method

--- back/src/main/java/com/example/back/repositories/RefreshTokenRepository.java
+++ back/src/main/java/com/example/back/repositories/RefreshTokenRepository.java
@@ -3,9 +3,17 @@ package com.example.back.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.back.entities.auth.RefreshToken;
+import com.example.back.entities.user.User;
+
+import java.time.Instant;
 import java.util.Optional;
 
-
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUser(User user);
+
     Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+
+    void deleteByExpiryDateBefore(Instant date);
 }
