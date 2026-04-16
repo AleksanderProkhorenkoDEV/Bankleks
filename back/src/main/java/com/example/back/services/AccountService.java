9578a0f086bc00ed00902d27347b5ad1fb1ee20d
aleksander <aprokhorenkodev@gmail.com>
2026-04-16 13:29:03 +0200

    fix: delete un used import temporaly

--- back/src/main/java/com/example/back/services/AccountService.java
+++ back/src/main/java/com/example/back/services/AccountService.java
@@ -1,11 +1,9 @@
 package com.example.back.services;
 
-import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.example.back.dto.transaction.account.CreateAccountRequestDTO;
-import com.example.back.entities.transaction.Transaction;
 import com.example.back.entities.transactions.Account;
 import com.example.back.entities.user.User;
 import com.example.back.repositories.AccountRepository;
