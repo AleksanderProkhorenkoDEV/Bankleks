56509e1476564af776de70911499039eb3c07f8b
aleksander <aprokhorenkodev@gmail.com>
2026-04-16 09:44:19 +0200

    fix: add repository annotation in the file

--- back/src/main/java/com/example/back/repositories/AccountRepository.java
+++ back/src/main/java/com/example/back/repositories/AccountRepository.java
@@ -2,7 +2,9 @@ package com.example.back.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.back.entities.transactions.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {}
--- back/src/main/java/com/example/back/repositories/TransactionRepository.java
+++ back/src/main/java/com/example/back/repositories/TransactionRepository.java
@@ -1,7 +1,9 @@
 package com.example.back.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.back.entities.transaction.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
