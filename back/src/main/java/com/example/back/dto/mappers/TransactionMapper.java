38f7e142d567f5ebe90875c35c861f159c331345
aleksander <aprokhorenkodev@gmail.com>
2026-04-16 13:27:24 +0200

    feat: create a Mapper to join differnt dto in one

---
+++ back/src/main/java/com/example/back/dto/mappers/TransactionMapper.java
@@ -0,0 +1,42 @@
+package com.example.back.dto.mappers;
+
+import org.springframework.stereotype.Component;
+
+import com.example.back.dto.transaction.account.AccountSummaryDTO;
+import com.example.back.dto.transaction.transaction.TransactionResponseDTO;
+import com.example.back.dto.user.UserSummaryDTO;
+import com.example.back.entities.transaction.Transaction;
+import com.example.back.entities.transactions.Account;
+import com.example.back.entities.user.User;
+
+@Component
+public class TransactionMapper {
+
+    public TransactionResponseDTO toDto(Transaction transaction) {
+        TransactionResponseDTO dto = new TransactionResponseDTO();
+
+        dto.setId(transaction.getId());
+        dto.setAmount(transaction.getAmount());
+        dto.setTransactionType(transaction.getType());
+        dto.setTransactionDate(transaction.getTransactionDay());
+        dto.setOriginAccount(toAccountSummary(transaction.getAccountOrigin()));
+        dto.setDestinationAccount(toAccountSummary(transaction.getAccountDestination()));
+        dto.setUser(toUserSummary(transaction.getUser()));
+
+        return dto;
+    }
+
+    private AccountSummaryDTO toAccountSummary(Account account) {
+        AccountSummaryDTO dto = new AccountSummaryDTO();
+        dto.setId(account.getId());
+        dto.setAccountNumber(account.getAccountNumber());
+        return dto;
+    }
+
+    private UserSummaryDTO toUserSummary(User user) {
+        UserSummaryDTO dto = new UserSummaryDTO();
+        dto.setId(user.getId());
+        dto.setUsername(user.getName());
+        return dto;
+    }
+}
