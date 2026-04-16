d494b6d74d93201d7111e6426f6292f0fbcb58a3
aleksander <aprokhorenkodev@gmail.com>
2026-04-16 10:25:30 +0200

    feat: override a method

--- back/src/main/java/com/example/back/repositories/TransactionRepository.java
+++ back/src/main/java/com/example/back/repositories/TransactionRepository.java
@@ -1,9 +1,14 @@
 package com.example.back.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.back.entities.transaction.Transaction;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {}
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    Page<Transaction> findAllByUserId(Long userId, Pageable pageable);
+}
