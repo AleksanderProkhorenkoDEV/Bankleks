f3ea3a5bc7dae00d060ee3abe3063d343d29edcf
aleksander <aprokhorenkodev@gmail.com>
2026-04-21 10:41:49 +0200

    feat: add new beans in to the base

--- back/src/test/java/com/example/back/controllers/transactions/account/AccountControllerBase.java
+++ back/src/test/java/com/example/back/controllers/transactions/account/AccountControllerBase.java
@@ -1,18 +1,17 @@
 package com.example.back.controllers.transactions.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.back.repositories.AccountRepository;
 import com.example.back.services.AccountService;
+import com.example.back.services.JwtService;
+import com.example.back.services.UserDetailsServiceImpl;
 import com.example.back.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-@SpringBootTest
-@AutoConfigureMockMvc
 public class AccountControllerBase {
     @Autowired
     protected MockMvc mockMvc;
@@ -25,5 +24,16 @@ public class AccountControllerBase {
 
     @MockitoBean
     protected AccountService accountService;
+
+    @MockitoBean
+    protected UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @MockitoBean
+    protected AuthenticationManager authenticationManager;
+
+    @MockitoBean
+    protected JwtService jwtService;
+
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
 }
