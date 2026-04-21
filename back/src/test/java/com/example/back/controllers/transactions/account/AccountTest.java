d519cd8b0b0a214ddd359c7c6f972c4795bff080
aleksander <aprokhorenkodev@gmail.com>
2026-04-21 10:41:12 +0200

    feat: create test to Account entity

---
+++ back/src/test/java/com/example/back/controllers/transactions/account/AccountTest.java
@@ -0,0 +1,37 @@
+package com.example.back.controllers.transactions.account;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.back.entities.auth.Role;
+import com.example.back.entities.transactions.Account;
+import com.example.back.entities.user.User;
+
+public class AccountTest {
+    User user = new User("user", "user@gmail.com", "12345678", new Role());
+
+    @Test
+    void shouldThrowIfBalanceIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Account(null, user, "1234567891234567899876"));
+    }
+
+    @Test
+    void shouldThrowIfUserIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Account(150.50, null, "1234567891234567899876"));
+    }
+
+    @Test
+    void shouldThrowIfAccountNumberIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Account(150.50, user, null));
+    }
+
+    @Test
+    void shouldCreateAccountWithValidData() {
+        Account account = new Account(150.50, user, "1234567891234567899876");
+        assertEquals(150.50, account.getBalance());
+        assertEquals(user, account.getUser());
+        assertEquals("1234567891234567899876", account.getAccountNumber());
+    }
+}
