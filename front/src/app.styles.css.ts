96236eef8ed4ff236cd15479e5ec416d7733fe2d
aleksander <aprokhorenkodev@gmail.com>
2026-04-23 13:07:20 +0200

    feat: add styles for main content

---
+++ front/src/app.styles.css.ts
@@ -0,0 +1,16 @@
+import { css } from "lit";
+
+export const appStyles = css` 
+    :host{
+        display:block;
+
+        height: 100svh;
+        
+        box-sizing:border-box;
+    }
+
+    main{
+        min-height: calc(100svh - 10.70svh);
+        overflow-y:auto;
+    }
+`
