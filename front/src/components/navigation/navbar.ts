ac107a8811d74fc0e9642d26a67e8bcadcdd5a5c
aleksander <aprokhorenkodev@gmail.com>
2026-04-23 11:34:34 +0200

    feat: create a custom event, wich the route have had clicked

--- front/src/components/navigation/navbar.ts
+++ front/src/components/navigation/navbar.ts
@@ -1,16 +1,35 @@
 import { customElement } from 'lit/decorators.js';
 import { LitElement, html } from 'lit';
+import { navBarRoutes } from '../../router/router';
 
 
 @customElement("nav-bar")
 export class NavBar extends LitElement {
+
+
+    handleRouteClick = (event: Event, route: string) => {
+        event.preventDefault();
+        this._navigate(route)
+    }
+
+    private _navigate = (href: String) => {
+        this.dispatchEvent(new CustomEvent('navigate', {
+            detail: { href },
+            bubbles: true,
+            composed: true
+        }))
+    }
+
     render() {
         return html`
             <header>
                 <h1>Bankleks</h1>
                 <nav>
-                    <a>Elemento 1</a>
-                    <a>Elemento 2</a>
+                    ${navBarRoutes.map((item) => {
+                        return html`
+                            <a href=${item.href} @click=${(event: Event) => this.handleRouteClick(event, item.href)}>${item.title}</a>
+                        `
+                    })}
                 </nav>
             </header>
         `
@@ -18,7 +37,7 @@ export class NavBar extends LitElement {
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'nav-bar': NavBar
-  }
+    interface HTMLElementTagNameMap {
+        'nav-bar': NavBar
+    }
 }
