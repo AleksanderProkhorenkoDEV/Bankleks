cd8cfb144f0fb254b2b0bac737a44c81bdd7b0f4
aleksander <aprokhorenkodev@gmail.com>
2026-04-27 13:11:50 +0200

    refactor: start to create the register page

--- front/src/pages/register.ts
+++ front/src/pages/register.ts
@@ -1,12 +1,29 @@
 import { customElement } from "lit/decorators.js";
 import { html, LitElement } from "lit";
+import { registerStyles } from "./register.styles";
 
 @customElement("register-page")
 export class RegisterPage extends LitElement {
+
+
+    static styles? = [
+        registerStyles,
+    ]
+
     render() {
         return html`
-            <section>
-                <h1>Register page</h1>
+            <section class="register">
+                <article class="register__item">
+                    <img 
+                        src="/candado.png" 
+                        width="350px" 
+                        alt="candado representativo de autorización"
+                        loading="lazy"
+                    />
+                </article>
+                <article class="register__item">
+                    <register-form></register-form>
+                </article>
             </section>
         `
     }
