75dce2eb36d0e9ea7613e2a487b9e0003ab10788
aleksander <aprokhorenkodev@gmail.com>
2026-04-27 15:47:48 +0200

    refactor: use the new componet

--- front/src/pages/register.ts
+++ front/src/pages/register.ts
@@ -1,30 +1,14 @@
 import { customElement } from "lit/decorators.js";
 import { html, LitElement } from "lit";
-import { registerStyles } from "./register.styles";
 
 @customElement("register-page")
 export class RegisterPage extends LitElement {
 
-
-    static styles? = [
-        registerStyles,
-    ]
-
     render() {
         return html`
-            <section class="register">
-                <article class="register__item">
-                    <img 
-                        src="/candado.png" 
-                        width="350px" 
-                        alt="candado representativo de autorización"
-                        loading="lazy"
-                    />
-                </article>
-                <article class="register__item">
-                    <register-form></register-form>
-                </article>
-            </section>
+            <layout-auth>
+                <register-form></register-form>
+            </layout-auth>
         `
     }
 }
