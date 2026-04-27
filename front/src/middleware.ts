74f1171c1b1b207e5c3726c24bac44eb1a98ba52
aleksander <aprokhorenkodev@gmail.com>
2026-04-27 16:44:47 +0200

    wonfix: testing the user

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -4,12 +4,15 @@ import { authService } from "./services/auth";
 export const middleware = (href: string): string => {
     const allRoutes = [...navBarRoutes, ...authRoutes]
     const route = allRoutes.find(route => route.href === href)
+    console.log('RUTA', route);
+    const user = authService.getUser();
+    console.log('user', user);
 
     if (!route) return '/not-found';
 
     if (route.public) return href;
 
-    const user = authService.getUser();
+
     if (!user) return '/signIn';
 
     if (route.roles && !route.roles.includes(user.role)) return '/not-found';
