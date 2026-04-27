4a3bba9eedc97f5b9ed04a01622f1891814f5abf
aleksander <aprokhorenkodev@gmail.com>
2026-04-27 13:47:47 +0200

    feat: create a service to call the auth endpoints

--- front/src/services/auth.ts
+++ front/src/services/auth.ts
@@ -1,10 +1,8 @@
-interface User {
-    id: string,
-    name: string,
-    role: 'client' | 'admin'
-}
+import type { RegisterBody, User } from "../types";
+import type { RegisterResponse } from "../types/auth";
+import { HttpService } from "./http";
 
-class AuthService {
+class AuthService extends HttpService {
     private _user: User | null = null;
 
     getUser(): User | null {
@@ -17,6 +15,18 @@ class AuthService {
 
     clear() {
         this._user = null;
+    }
+
+
+    async register(body: RegisterBody): Promise<{ ok: boolean, error?: string }> {
+        console.log('BODY QUE ENVIAMOS', body);
+        
+        try {
+            await this.post<RegisterResponse>('/auth/register', body);
+            return { ok: true };
+        } catch (error: any) {
+            return { ok: false, error: error.message };
+        }
     }
 }
 
