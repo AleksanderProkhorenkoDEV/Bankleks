d42e1a609e7dab216f53917287ac59021b3668a0
aleksander <aprokhorenkodev@gmail.com>
2026-04-27 13:12:18 +0200

    feat: create a custom component for buttons

---
+++ front/src/components/forms/parts/buttons.ts
@@ -0,0 +1,41 @@
+import { customElement, property } from "lit/decorators.js";
+import { buttonStyles } from "./button.styles";
+import { html, LitElement } from "lit";
+
+@customElement("button-form")
+export class ButtonForm extends LitElement {
+
+
+    @property() variant: 'primary' | 'secondary' | 'danger' = 'primary';
+    @property() type: 'submit' | 'button' | 'reset' = 'button';
+    @property({ type: Boolean }) disabled: boolean = false;
+
+    private _handleClick = () => {
+        if (this.type === 'submit') {
+            const form = this.closest('form');
+            form?.requestSubmit();
+        }
+    }
+
+    static styles = [
+        buttonStyles,
+    ]
+
+    render() {
+        return html`
+            <button 
+                type=${this.type}
+                ?disabled=${this.disabled}
+                @click=${this._handleClick}
+            >
+                <slot></slot>
+            </button>
+        `
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "button-form": ButtonForm
+    }
+}
