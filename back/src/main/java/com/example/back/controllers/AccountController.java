5668094cf3e53371258319b3943e1eb9b449c065
aleksander <aprokhorenkodev@gmail.com>
2026-04-28 16:24:38 +0200

    refactor: change the response to facility the display data in front

--- back/src/main/java/com/example/back/controllers/AccountController.java
+++ back/src/main/java/com/example/back/controllers/AccountController.java
@@ -3,6 +3,8 @@ package com.example.back.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.back.dto.mappers.AccountMapper;
+import com.example.back.dto.transaction.account.AccountResponseDTO;
 import com.example.back.dto.transaction.account.GetBalanceResponseDTO;
 import com.example.back.entities.transactions.Account;
 import com.example.back.services.AccountService;
@@ -11,21 +13,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
 
-    private AccountService accountService;
+    private final AccountService accountService;
+    private final AccountMapper accountMapper;
 
-    public AccountController(AccountService accountService) {
+    public AccountController(AccountService accountService, AccountMapper accountMapper) {
         this.accountService = accountService;
+        this.accountMapper = accountMapper;
     }
 
     @GetMapping("/{id}/balance")
     public ResponseEntity<GetBalanceResponseDTO> getBalanceAccount(@PathVariable Long id) {
         Account account = this.accountService.getAccount(id);
         return ResponseEntity.ok(new GetBalanceResponseDTO(account.getBalance()));
+    }
+
+    @GetMapping("/{id}/all-data")
+    public ResponseEntity<AccountResponseDTO> getMethodName(@PathVariable Long id) {
+        Account account = accountService.getAccount(id);
+        return ResponseEntity.ok(accountMapper.toDTO(account));
     }
 
 }
