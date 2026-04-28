704bb6130cd6c0607cb19cd520bc24e28a167e84
aleksander <aprokhorenkodev@gmail.com>
2026-04-28 09:33:35 +0200

    feat: delete console, and use the storage

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,21 +1,19 @@
 import { authRoutes, navBarRoutes } from "./router/router"
-import { authService } from "./services/auth";
+import { authStore } from "./store/auth";
 
 export const middleware = (href: string): string => {
     const allRoutes = [...navBarRoutes, ...authRoutes]
     const route = allRoutes.find(route => route.href === href)
-    console.log('RUTA', route);
-    const user = authService.getUser();
-    console.log('user', user);
-
+    
     if (!route) return '/not-found';
-
+    
     if (route.public) return href;
 
+    const { user } = authStore.getState();
 
     if (!user) return '/signIn';
-
-    if (route.roles && !route.roles.includes(user.role)) return '/not-found';
+     
+    if (route.roles && !route.roles.includes(user.role)) return '/not-found';   
 
     return href;
 }
