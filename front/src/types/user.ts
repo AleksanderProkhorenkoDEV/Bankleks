2b9265e6cd976b6202778ffc093a8ed46585c032
aleksander <aprokhorenkodev@gmail.com>
2026-04-30 12:46:03 +0200

    feat: add new type

--- front/src/types/user.ts
+++ front/src/types/user.ts
@@ -2,3 +2,10 @@ export interface UserSummary {
     id: number,
     username: string;
 }
+
+export interface UserResponse {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+}
