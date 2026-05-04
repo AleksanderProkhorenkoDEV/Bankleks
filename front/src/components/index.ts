19c521f73549de3ef4f75bd18697441fb4f83f44
aleksander <aprokhorenkodev@gmail.com>
2026-05-04 10:05:02 +0200

    feat: export new pages

--- front/src/components/index.ts
+++ front/src/components/index.ts
@@ -1,9 +1,9 @@
 //Barrel export, to import all components in main index.html
 
 export { ButtonForm, InputForm, RegisterForm, SignInForm, TransactionForm } from "./forms";
+export { LayoutAuth, NotFoundPage, LoadingScreen, ErrorScreen } from "./layout"
 export { NavBar, NavigateLink } from "./navigation"
+export { LineChart, DonutChart } from "./charts"
 export { EditTransactionModal } from "./modals"
-export { LayoutAuth } from "./layout"
 export { ToastMessage } from "./toast"
 export { DataTable } from "./table"
-export { LineChart, DonutChart } from "./charts"
