134579d0f89b5aa90a62729e8ef049b5caae0eb4
aleksander <aprokhorenkodev@gmail.com>
2026-05-07 12:42:30 +0200

    feat: add new setupmockmvc method to pass the auth

--- back/src/test/java/com/example/back/controllers/auth/AuthControllerBase.java
+++ back/src/test/java/com/example/back/controllers/auth/AuthControllerBase.java
@@ -1,26 +1,41 @@
 package com.example.back.controllers.auth;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-import com.example.back.entities.user.User;
+import com.example.back.controllers.AuthController;
+import com.example.back.controllers.GlobalExceptionController;
 import com.example.back.repositories.RefreshTokenRepository;
 import com.example.back.repositories.UserRepository;
+import com.example.back.services.AuthService;
 import com.example.back.services.JwtService;
 import com.example.back.services.RefreshTokenService;
+import com.example.back.services.UserDetailsServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = AuthController.class)
+@Import(GlobalExceptionController.class)
 abstract class AuthControllerBase {
-    @Autowired
-    protected MockMvc mockMvc;
 
+    @Autowired
+    private AuthController authController;
+
+    @Autowired
+    private GlobalExceptionController globalExceptionController;
+
+    protected MockMvc mockMvc;
     protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockitoBean
+    protected AuthService authService;
 
     @MockitoBean
     protected AuthenticationManager authenticationManager;
@@ -32,12 +47,20 @@ abstract class AuthControllerBase {
     protected RefreshTokenService refreshTokenService;
 
     @MockitoBean
+    protected UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @MockitoBean
     protected UserRepository userRepository;
 
     @MockitoBean
     protected RefreshTokenRepository refreshTokenRepository;
 
-    @MockitoBean
-    User user;
-
+    @BeforeEach
+    void setUpMockMvc() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(authController, globalExceptionController)
+                .setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver())
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
 }
