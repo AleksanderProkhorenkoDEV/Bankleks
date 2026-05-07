aafca94e2d124f8391b4c466f3fb722c15661eca
aleksander <aprokhorenkodev@gmail.com>
2026-05-07 12:42:01 +0200

    refactor: simplify the test

--- back/src/test/java/com/example/back/controllers/auth/AuthControllerLogoutTest.java
+++ back/src/test/java/com/example/back/controllers/auth/AuthControllerLogoutTest.java
@@ -14,29 +14,9 @@ import com.example.back.entities.auth.RefreshToken;
 import jakarta.servlet.http.Cookie;
 
 class AuthControllerLogoutTest extends AuthControllerBase {
-        @Test
-        void shouldReturn400WhenRefreshTokenIsInvalid() throws Exception {
-
-                when(refreshTokenRepository.findByToken("refresh-token-fake"))
-                                .thenReturn(Optional.empty());
-
-                mockMvc.perform(post("/auth/logout")
-                                .cookie(new Cookie("refreshToken", "refresh-token-fake")))
-                                .andExpect(status().isBadRequest())
-                                .andExpect(jsonPath("$.message")
-                                                .value("Refresh token inválido."));
-        }
-
-        @Test
-        void shouldReturn400WhenCookieIsMissing() throws Exception {
-
-                mockMvc.perform(post("/auth/logout"))
-                                .andExpect(status().isBadRequest());
-        }
 
         @Test
         void shouldReturn200WhenRefreshTokenIsValid() throws Exception {
-
                 RefreshToken token = new RefreshToken();
                 token.setToken("refresh-token-valid");
 
@@ -46,7 +26,23 @@ class AuthControllerLogoutTest extends AuthControllerBase {
                 mockMvc.perform(post("/auth/logout")
                                 .cookie(new Cookie("refreshToken", "refresh-token-valid")))
                                 .andExpect(status().isOk())
-                                .andExpect(jsonPath("$.message")
-                                                .value("Sesión cerrada"));
+                                .andExpect(jsonPath("$.message").value("Sesión cerrada"));
+        }
+
+        @Test
+        void shouldReturn200WhenRefreshTokenNotFound() throws Exception {
+                when(refreshTokenRepository.findByToken("refresh-token-fake"))
+                                .thenReturn(Optional.empty());
+
+                mockMvc.perform(post("/auth/logout")
+                                .cookie(new Cookie("refreshToken", "refresh-token-fake")))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.message").value("Sesión cerrada"));
+        }
+
+        @Test
+        void shouldReturn400WhenCookieIsMissing() throws Exception {
+                mockMvc.perform(post("/auth/logout"))
+                                .andExpect(status().isBadRequest());
         }
 }
