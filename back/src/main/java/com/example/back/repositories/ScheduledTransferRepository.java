6e829cf7c31abed323b1b9633c8c91cd66683e42
aleksander <aprokhorenkodev@gmail.com>
2026-05-11 12:12:38 +0200

    feat: add method to get the transaction

--- back/src/main/java/com/example/back/repositories/ScheduledTransferRepository.java
+++ back/src/main/java/com/example/back/repositories/ScheduledTransferRepository.java
@@ -1,9 +1,26 @@
 package com.example.back.repositories;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.back.entities.transactions.Account;
 import com.example.back.entities.transactions.ScheduledTransfer;
+import com.example.back.enums.ScheduledTransactionType;
 
 @Repository
-public interface ScheduledTransferRepository extends JpaRepository<ScheduledTransfer, Long> {}
+public interface ScheduledTransferRepository extends JpaRepository<ScheduledTransfer, Long> {
+
+    List<ScheduledTransfer> findByStatusAndScheduledAtBefore(
+            ScheduledTransactionType status, Instant now);
+
+    List<ScheduledTransfer> findByAccountOriginAndStatus(
+            Account account, ScheduledTransactionType status);
+
+    List<ScheduledTransfer> findByAccountOrigin(Account account);
+
+    List<ScheduledTransfer> findByStatusAndCreatedAtBefore(
+            ScheduledTransactionType status, Instant threshold);
+}
