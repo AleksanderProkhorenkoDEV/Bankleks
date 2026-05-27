672ddbf48f287493cf65cb28aeb842e6e9a5c782
aleksander <aprokhorenkodev@gmail.com>
2026-05-27 09:27:22 +0200

    feat: add new recurrence types

--- back/src/main/java/com/example/back/enums/RecurrenceType.java
+++ back/src/main/java/com/example/back/enums/RecurrenceType.java
@@ -3,5 +3,8 @@ package com.example.back.enums;
 public enum RecurrenceType {
     BEGINNING_OF_MONTH,
     MIDDLE_OF_MONTH,
-    END_OF_MONTH
+    END_OF_MONTH,
+    EVERY_X_DAYS,
+    EVERY_X_WEEKS,
+    EVERY_X_MONTHS,
 }
